a249d3d358eb46d03ede72a777d281d914a5fa11
AlexanderGorgonzola <alex.garakanidze@protonmail.com>
2024-07-22 11:06:56 -0700

    main

---
+++ settings.py
@@ -0,0 +1,7 @@
+class Settings:
+    def __init__(self):
+        self.screen_width = 1200
+        self.screen_length = 800
+        self.bg_color = (94, 50, 50)
+        
+        self.player_speed = 0.5
